8e24b3366a49da2d9654724ec8f4d98d7f4fb4d8
adesh-ghodekar <adeshghodekar23@gmail.com>
2026-07-22 14:50:42 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.56%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,25 +1,36 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int j=-1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==val){
-                j=i;
-                break;
-            }else if(nums[i]!=val && i==nums.length-1){
-                return nums.length;
+        // int j=-1;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==val){
+        //         j=i;
+        //         break;
+        //     }else if(nums[i]!=val && i==nums.length-1){
+        //         return nums.length;
+        //     }
+        // }
+        // if(j==-1){
+        //     return 0;
+        // }
+        // for(int i=j+1;i<nums.length;i++){
+        //     if(nums[i]!=val){
+        //         int temp = nums[i];
+        //         nums[i]=nums[j];
+        //         nums[j]=temp;
+        //         j++;
+        //     }
+        // }
+        // return j;
+
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
             }
         }
-        if(j==-1){
-            return 0;
-        }
-        for(int i=j+1;i<nums.length;i++){
-            if(nums[i]!=val){
-                int temp = nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                j++;
-            }
-        }
-        return j;
+
+        return k;
     }
 }
